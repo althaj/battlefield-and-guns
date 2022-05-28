5da476cd2ffffbf8a64f387b9e0a53eb314eda8a
Dávid Florek <33327502+althaj@users.noreply.github.com>
2022-05-28 08:48:01 +0200

    Mars map tiles + Map pieces

---
+++ Assets/Scripts/Map/MapBuilder.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PSG.BattlefieldAndGuns.Map
+{
+    public class MapBuilder : MonoBehaviour
+    {
+        #region serialized variables
+
+        [SerializeField] private GameObject[] straightPrefabs;
+        [SerializeField] private GameObject[] corners;
+
+        #endregion
+
+        #region private variables
+
+        #endregion
+
+        #region properties
+
+        #endregion
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
